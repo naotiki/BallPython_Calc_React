ba70021c5285003ec5931b28314ea086df9c8708
Naotiki <razupai709@gmail.com>
2021-03-31 21:07:24 +0900

    Sortを忘れてたので修正 JS嫌い

--- src/main/resources/calc.js
+++ src/main/resources/calc.js
@@ -8,21 +8,32 @@ self.addEventListener('message', function (e) {
     let male = obj.first;//Array
     let female = obj.second;// "
     let pairs = [ /*[[0,0],[0,0]]*/];
+    let denyIndex = [];
     male.forEach(function (element) {
+        if (denyIndex.includes(male.indexOf(element))) {
+            return
+        }
         let i = getIndex(female, element)
         console.log(i)
         if (i !== -1) {
             pairs.push([[element.first, element.second], [female[i].first, female[i].second]]);
             female.splice(i, 1);
         } else {
-            pairs.push([[element.first, element.second], [0, 0]]);
+            let index = getIndex(male, element)
+            if (index !== -1) {
+                pairs.push([[element.first, element.second], [male[i].first, male[i].second]]);
+                denyIndex.push(index)
+            } else {
+
+                pairs.push([[element.first, element.second], [0, 0]]);
+            }
         }
     });
     female.forEach(function (element) {
         pairs.push([[element.first, element.second], [0, 0]]);
     });
     if (pairs.length !== 1) {
-        pairs = pairs.filter(it => it[0] !== [0,0]);
+        pairs = pairs.filter(it => it[0] !== [0, 0]);
     }
     console.log(pairs)
     let all = Math.pow(4, pairs.length);
@@ -45,7 +56,7 @@ self.addEventListener('message', function (e) {
             for (let j = 0; j < all / Math.pow(4, i + 1); j++) {
 
 
-                children[index].push(groups[groupIndex])
+                children[index].push(groups[groupIndex].sort(compareDesc))
                 index++
             }
             groupIndex++
@@ -58,10 +69,12 @@ self.addEventListener('message', function (e) {
     //処理結果を送信
     self.postMessage(JSON.stringify(children));
 }, false);
-
+function compareDesc(a, b) {
+    return b - a;
+}
 function getIndex(array, value) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i].first === value.first) {
+        if (array[i].first === value.first && value !== array[i]) {
 
             return i
         }
