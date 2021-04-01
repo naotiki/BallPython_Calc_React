996d88786d5c998a832929b0da90da1b42b36942
Naotiki <razupai709@gmail.com>
2021-04-01 18:30:36 +0900

    FIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIX

--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -2,46 +2,48 @@ import GeneticPair.Companion.ToGeneticPairFromID
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Snake(var pairs:List<GeneticPair>){
+data class Snake(var pairs: List<GeneticPair>) {
 
 
     constructor() : this(emptyList<GeneticPair>())
 
-    fun ConvertToInt():List<Pair<Int,Int>>{
-        val r= mutableListOf<Pair<Int,Int>>()
+    fun ConvertToInt(): List<Pair<Int, Int>> {
+        val r = mutableListOf<Pair<Int, Int>>()
         pairs.forEach {
             r.add(it.toIntPair())
         }
         return r
     }
 
-    fun addFromId(a:Pair<Int,Int>){
-        val intPair= if (a.first < a.second) a.second to a.first else a
-        val ml= pairs.toMutableList()
+    fun addFromId(a: Pair<Int, Int>) {
+        val intPair = if (a.first < a.second) a.second to a.first else a
+        val ml = pairs.toMutableList()
         ml.add(ToGeneticPairFromID(intPair))
 
-        pairs=ml
+        pairs = ml
 
     }
 
     override fun toString(): String {
-        var str=""
-       val tmp= pairs.sortedBy { it.mainGenetic.id}
+        var str = ""
+        val tmp = pairs.sortedBy { it.mainGenetic.id }
         tmp.forEach {
-            str+=it.toString()
+            str += "$it "
         }
-        str=str.replace("ノーマル","")
-       str= if(str!="") str else  "ノーマル"
+        str = str.trimEnd()
+        str = str.replace("ノーマル", "")
+
+        str = if (str.replace(" ","") != "") str else "ノーマル"
         return str
 
     }
 
-    companion object{
+    companion object {
         fun Convert(list: List<List<Int>>): Snake {
 
-            val r= mutableListOf<GeneticPair>()
+            val r = mutableListOf<GeneticPair>()
             list.forEach {
-                r.add( ToGeneticPairFromID(Pair(it[0],it[1])))
+                r.add(ToGeneticPairFromID(Pair(it[0], it[1])))
             }
             return Snake(r)
         }
